d8dd8de8d05fbc23b7077c9290d0954c97a63126
Bastien Aracil <findbastien@gmail.com>
2011-12-26 12:29:06 +0100

    Add Copyrights for FPCWordPress plugin

--- FPCWordPress/FPCWordPress/FPCWordPress.php
+++ FPCWordPress/FPCWordPress/FPCWordPress.php
@@ -25,7 +25,7 @@
  *   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  *   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
- *   @package FPC_AMFPHP_Plugins_ServiceAccess
+ *   @package FPC_AMFPHP_Plugins_FPCWordPress
  */
 require_once "ClassLoader.php";
 
